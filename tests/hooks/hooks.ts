e416360170b65d72a76210c9f9417011f2e31579
CJ <cjcpineda@gmail.com>
2026-02-19 16:34:47 -0500

    fixing timeout error

--- tests/hooks/hooks.ts
+++ tests/hooks/hooks.ts
@@ -1,9 +1,11 @@
-import { Before, After, Status } from '@cucumber/cucumber';
+import { Before, After, Status, setDefaultTimeout } from '@cucumber/cucumber';
 import { chromium } from '@playwright/test';
 import { CustomWorld } from '../world/custom-world';
 import { PickleballHomePage } from '../pages/pbGeneral-page';
 
-Before({ timeout: 10000 }, async function (this: CustomWorld) {
+setDefaultTimeout(30 * 1000);
+
+Before(async function (this: CustomWorld) {
     this.browser = await chromium.launch({ headless: false });
     this.context = await this.browser.newContext();
     this.page = await this.context.newPage();
